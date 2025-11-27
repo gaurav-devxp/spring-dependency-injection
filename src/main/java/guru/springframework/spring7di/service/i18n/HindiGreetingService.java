26b85472b937d4e5123ee94545c4ef3ea1a39a2c
Gaurav Pandey <gauravdev.xp@gmail.com>
2025-11-27 14:19:14 +0530

    Add Default Profiles

--- src/main/java/guru/springframework/spring7di/service/i18n/HindiGreetingService.java
+++ src/main/java/guru/springframework/spring7di/service/i18n/HindiGreetingService.java
@@ -4,7 +4,7 @@ import guru.springframework.spring7di.service.GreetingService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("HI")
+@Profile({"HI", "default"})
 @Service("i18NService")
 public class HindiGreetingService implements GreetingService {
     @Override
